2ecc0977ee70c3528592e1586c0929303c97666e
PhuChau <vinhphu101195@gmail.com>
2019-06-13 17:48:38 +0300

    Schema and mongoose

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 //set up express app
 const app = express();
+
+app.use(bodyParser.json());
+
+//initialize routes
+app.use("/api", require("./routes/api"));
 
 app.listen(process.env.port || 3000, function() {
   console.log("now listening for request");
---
+++ models/ninja.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// create ninja Schema & model
+const NinjaSchema = new Schema({
+  name: {
+    type: String,
+    required: [true, "Name field is required"]
+  },
+  rank: {
+    type: String
+  },
+  available: {
+    type: Boolean,
+    default: false
+  }
+});
